ecc2cf3374fad233621d02c4080890f5620edfc8
danishppatel <danishpatel5113@gmail.com>
2024-07-12 11:50:01 +0530

    Lab-2

---
+++ Calender/Calender/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Calender._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <style>
+        .other-month {
+            background-color: gray;
+        }
+        .weekend {
+            background-color: yellow;
+        }
+    </style>
+
+    <main>
+        <div>
+            <asp:TextBox ID="SelectedDateTextBox" runat="server"></asp:TextBox>
+            <asp:Button ID="ShowCalendarButton" runat="server" Text="Show Calendar" OnClick="ShowCalendarButton_Click" /> <br><br>
+            <asp:Calendar ID="CalendarControl" runat="server" OnSelectionChanged="CalendarControl_SelectionChanged" OnDayRender="CalendarControl_DayRender" Visible="false"></asp:Calendar>
+        </div>
+    </main>
+
+</asp:Content>
---
+++ Calender/Calender/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calender
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void ShowCalendarButton_Click(object sender, EventArgs e)
+        {
+            CalendarControl.Visible = true;
+        }
+
+        protected void CalendarControl_SelectionChanged(object sender, EventArgs e)
+        {
+            DateTime selectedDate = CalendarControl.SelectedDate;
+            if (selectedDate >= new DateTime(2024, 7, 29) && selectedDate <= new DateTime(2024, 8, 3))
+            {
+                SelectedDateTextBox.Text = "Sessional Week";
+            }
+            else
+            {
+                SelectedDateTextBox.Text = selectedDate.ToShortDateString();
+            }
+        }
+
+        protected void CalendarControl_DayRender(object sender, DayRenderEventArgs e)
+        {
+            if (e.Day.IsOtherMonth)
+            {
+                e.Cell.CssClass = "other-month";
+            }
+            else if (e.Day.IsWeekend)
+            {
+                e.Cell.CssClass = "weekend";
+            }
+
+            if (e.Day.Date.Month == DateTime.Now.Month && e.Day.Date.Day == 5)
+            {
+                e.Cell.Text = "It's Birthday";
+            }
+        }
+    }
+}
